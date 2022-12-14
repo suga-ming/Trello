35f327936b36496ff2b850ba003b062e63e6556e
suga-ming <kby9901@naver.com>
2022-12-14 13:02:43 +0900

    atom

--- src/store/atom.tsx
+++ src/store/atom.tsx
@@ -1,4 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "todoLocal",
+  storage: localStorage,
+});
 
 export interface ITodo {
   id: number;
@@ -9,6 +15,12 @@ interface IToDoState {
   [key: string]: ITodo[];
 }
 
+// export interface IToDoState {
+//   id: number;
+//   title: string;
+//   toDos: ITodo[];
+// }
+
 export const toDoState = atom<IToDoState>({
   key: "toDo",
   default: {
@@ -17,3 +29,25 @@ export const toDoState = atom<IToDoState>({
     done: [],
   },
 });
+
+// export const toDoState = atom<IToDoState[]>({
+//   key: "toDo",
+//   default: [
+//     {
+//       id: 1, //boardId
+//       title: "todo", //카테고리 제목
+//       toDos: [], //해당 카테고리에 할일 들
+//     },
+//     {
+//       id: 2,
+//       title: "doing",
+//       toDos: [],
+//     },
+//     {
+//       id: 3,
+//       title: "done",
+//       toDos: [],
+//     },
+//   ],
+//   effects_UNSTABLE: [persistAtom],
+// });
